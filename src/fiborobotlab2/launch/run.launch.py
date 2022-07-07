7acb1bb30855a63c72e4c3a9ace0d757bfca6543
tharittapol <57582928+tharittapol@users.noreply.github.com>
2022-07-07 14:47:42 +0700

    Big's CHANGE

--- src/fiborobotlab2/launch/run.launch.py
+++ src/fiborobotlab2/launch/run.launch.py
@@ -15,14 +15,26 @@ import simplejson
 config_robot = simplejson.load(open(os.path.join(get_package_share_directory('fiborobotlab2'), 'config', 'robotname.json')))
 robotname=config_robot["robotname"]
 def generate_launch_description():
-    return LaunchDescription([
-        Node(package='dynamixel_sdk_examples',executable='read_write_node',name='read_write_node_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='detection_node.py',name='Detection_Node_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='PID_Node.py',name='PID_node_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='State_Controll.py',name='State_Controll_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='Follow_ball.py',name='Fllow_ball_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='camera.py',name='camera_publisher_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='prepare_kick.py',name='Prepare_Kick_{}'.format(robotname)),
-        Node(package='fiborobotlab2',executable='Kick.py',name='Kick_{}'.format(robotname)),
-
-    ])
+    if robotname != 'robot4':
+        return LaunchDescription([
+            Node(package='dynamixel_sdk_examples',executable='read_write_node',name='read_write_node_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='detection_node.py',name='Detection_Node_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='PID_Node.py',name='PID_node_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='State_Controll.py',name='State_Controll_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='Follow_ball.py',name='Fllow_ball_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='camera.py',name='camera_publisher_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='prepare_kick.py',name='Prepare_Kick_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='Kick.py',name='Kick_{}'.format(robotname)),
+        ])
+    else:
+        return LaunchDescription([
+            #Node(package='dynamixel_sdk_examples',executable='read_write_node',name='read_write_node_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='detection_node.py',name='Detection_Node_{}'.format(robotname)),
+            #Node(package='fiborobotlab2',executable='PID_Node.py',name='PID_node_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='State_Controll.py',name='State_Controll_{}'.format(robotname)),
+            #Node(package='fiborobotlab2',executable='Follow_ball.py',name='Fllow_ball_{}'.format(robotname)),
+            #Node(package='fiborobotlab2',executable='camera.py',name='camera_publisher_{}'.format(robotname)),
+            #Node(package='fiborobotlab2',executable='prepare_kick.py',name='Prepare_Kick_{}'.format(robotname)),
+            #Node(package='fiborobotlab2',executable='Kick.py',name='Kick_{}'.format(robotname)),
+            Node(package='fiborobotlab2',executable='Goalkeeper2.py',name='Goalkeeper2_{}'.format(robotname)),
+        ])
